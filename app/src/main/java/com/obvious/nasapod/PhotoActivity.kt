b974d24530256c50634c110b5256d38c24db5c3b
sidJr <sidJr@thumbmunkeys.com>
2019-09-08 19:37:22 +0530

    pan and zoom image func. implemented in detail screen

--- app/src/main/java/com/obvious/nasapod/PhotoActivity.kt
+++ app/src/main/java/com/obvious/nasapod/PhotoActivity.kt
@@ -17,6 +17,7 @@ class PhotoActivity : AppCompatActivity() {
         selectedPhoto = intent.getSerializableExtra(Image_key) as NasaPhoto
         Picasso.with(this).load(selectedPhoto?.url).into(photoImageView)
 
+//        var imageView = CustomImageView(this)
         photoTitle.text = selectedPhoto?.title
         photoDate.text = selectedPhoto?.date
         photoDescription.text = selectedPhoto?.explanation
